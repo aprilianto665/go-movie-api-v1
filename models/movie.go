ea7bff8cf708df0167cd62d1428fc1e1d8d1369e
Muhammad Aprilianto <aprilianto665@gmail.com>
2025-08-16 19:39:33 +0700

    creating movie struct

--- models/movie.go
+++ models/movie.go
@@ -5,7 +5,16 @@ import "database/sql"
 type MovieController struct{ db *sql.DB }
 
 type Movie struct {
-    ID    int64  `json:"id"`
-    Title string `json:"title"`
-    Year  int    `json:"year"`
+    ID          int      `json:"id"`
+    Title       string   `json:"title"`
+    Image       string   `json:"image"`
+    Category    string   `json:"category"`
+    Genre       []string `json:"genre"`
+    Duration    int      `json:"duration"`
+    ReleaseYear int      `json:"releaseYear"`
+    AgeRating   int      `json:"ageRating"`
+    Synopsis    string   `json:"synopsis"`
+    Cast        []string `json:"cast"`
+    Creators    []string `json:"creators"`
+    Rating      float64  `json:"rating"`
 }
